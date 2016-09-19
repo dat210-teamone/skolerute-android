3fbedf5598f64b3085ba06078588c34731b87070
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2016-09-19 11:15:51 +0200

    Added a new Test

---
+++ app/src/test/java/com/github/dat210_teamone/skolerute/StorageInterfaceTest.java
@@ -0,0 +1,23 @@
+package com.github.dat210_teamone.skolerute;
+
+import com.github.dat210_teamone.skolerute.data.DummyStorage;
+import com.github.dat210_teamone.skolerute.data.StorageInterface;
+import com.github.dat210_teamone.skolerute.model.SchoolInfo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by Nicolas on 19.09.2016.
+ */
+public class StorageInterfaceTest {
+    @Test
+    public void TestRetrieveAll() throws Exception{
+        StorageInterface si = new DummyStorage();
+
+        SchoolInfo[] test = si.GetSchoolInfo();
+        Assert.assertEquals(10, test.length);
+    }
+}
