e79b73fbebca1fb68ca3461211ef048c57752c94
Espen <espenvie@broadpark.no>
2016-09-19 10:24:32 +0200

    Dummy storage start

---
+++ app/src/main/java/com/github/dat210_teamone/skolerute/data/DummyStorage.java
@@ -0,0 +1,18 @@
+package com.github.dat210_teamone.skolerute.data;
+
+import com.github.dat210_teamone.skolerute.model.SchoolInfo;
+import com.github.dat210_teamone.skolerute.model.SchoolVacationDay;
+
+import java.util.ArrayList;
+
+/**
+ * Created by espen on 19.09.16.
+ */
+public class DummyStorage {
+    private ArrayList<SchoolInfo> schoolInfo;
+    private ArrayList<SchoolVacationDay> schoolVacationDay;
+
+    public DummyStorage() {
+
+    }
+}
