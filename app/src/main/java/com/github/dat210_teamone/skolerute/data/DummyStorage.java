214dacd1af29b53272b81a0b1ef72e804457f8c2
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2016-09-19 10:37:51 +0200

    Implemented storageInterface for DummyStorage

--- app/src/main/java/com/github/dat210_teamone/skolerute/data/DummyStorage.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/data/DummyStorage.java
@@ -1,5 +1,6 @@
 package com.github.dat210_teamone.skolerute.data;
 
+import com.android.internal.util.Predicate;
 import com.github.dat210_teamone.skolerute.model.SchoolInfo;
 import com.github.dat210_teamone.skolerute.model.SchoolVacationDay;
 
@@ -8,11 +9,45 @@ import java.util.ArrayList;
 /**
  * Created by espen on 19.09.16.
  */
-public class DummyStorage {
+public class DummyStorage implements StorageInterface {
     private ArrayList<SchoolInfo> schoolInfo;
     private ArrayList<SchoolVacationDay> schoolVacationDay;
 
     public DummyStorage() {
 
+    }
+
+    @Override
+    public SchoolInfo[] GetSchoolInfo() {
+        return (SchoolInfo[])schoolInfo.toArray();
+    }
+
+    @Override
+    public SchoolVacationDay[] GetVacationDays() {
+        return (SchoolVacationDay[])schoolVacationDay.toArray();
+    }
+
+    @Override
+    public SchoolInfo[] GetSchoolInfo(Predicate<SchoolInfo> func) {
+        ArrayList<SchoolInfo> filter = new ArrayList<SchoolInfo>();
+        for (SchoolInfo obj : schoolInfo)
+        {
+            if (func.apply(obj)) {
+                filter.add(obj);
+            }
+        }
+        return (SchoolInfo[])filter.toArray();
+    }
+
+    @Override
+    public SchoolVacationDay[] GetVacationDays(Predicate<SchoolVacationDay> func) {
+        ArrayList<SchoolVacationDay> filter = new ArrayList<SchoolVacationDay>();
+        for (SchoolVacationDay obj : schoolVacationDay)
+        {
+            if (func.apply(obj)) {
+                filter.add(obj);
+            }
+        }
+        return (SchoolVacationDay[])filter.toArray();
     }
 }
