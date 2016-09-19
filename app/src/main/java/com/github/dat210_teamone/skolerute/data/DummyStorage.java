c31a554a02af0066bc480cd38189bb60fcf7548e
espen <espenvie@broadpark.no>
2016-09-19 10:44:58 +0200

    Dummy stuff

--- app/src/main/java/com/github/dat210_teamone/skolerute/data/DummyStorage.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/data/DummyStorage.java
@@ -14,7 +14,18 @@ public class DummyStorage implements StorageInterface {
     private ArrayList<SchoolVacationDay> schoolVacationDay;
 
     public DummyStorage() {
-
+        for(int i = 0; i < 10; i++) {
+            SchoolInfo dummySchool = new SchoolInfo();
+            dummySchool.setNorth(1234567.11);
+            dummySchool.setEast(7654321.99);
+            dummySchool.setLatitude(58.946442);
+            dummySchool.setLongitude(5.726693);
+            dummySchool.setSchoolName("Skole " + i);
+            dummySchool.setAddress("Address " + i);
+            dummySchool.setHomePage("www.school" + i +".com");
+            dummySchool.setSudents("ELEVER/TRINN 1.-51  2.-68  3.-51  4.- 69  5.-49  6.-45  7.-41");
+            dummySchool.setCapacity("" + 10+i + "klasserom");
+        }
     }
 
     @Override
