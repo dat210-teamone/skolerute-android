66e4e38a6ded0b9a71f86eb856ca69b707c8d29e
jerzyjje <jerzyjje@gmail.com>
2016-09-19 11:33:54 +0200

    Buttons

--- app/src/main/java/com/github/dat210_teamone/skolerute/calendar.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/calendar.java
@@ -2,6 +2,8 @@ package com.github.dat210_teamone.skolerute;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class calendar extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+    }
+
+    public void changeView(View view) {
+        Intent myIntent = new Intent(this, CalendarList.class);
+        startActivity(myIntent);
+    }
+
+    public void showSchools(View view) {
+
     }
 }
