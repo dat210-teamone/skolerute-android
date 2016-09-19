92b931495c3d2fae603544433aa9dd6b4179a068
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-09-19 10:28:19 +0200

    Test Retrieve All Vacation Days added.

--- app/src/test/java/com/github/dat210_teamone/skolerute/StorageInterfaceTest.java
+++ app/src/test/java/com/github/dat210_teamone/skolerute/StorageInterfaceTest.java
@@ -3,6 +3,7 @@ package com.github.dat210_teamone.skolerute;
 import com.github.dat210_teamone.skolerute.data.DummyStorage;
 import com.github.dat210_teamone.skolerute.data.StorageInterface;
 import com.github.dat210_teamone.skolerute.model.SchoolInfo;
+import com.github.dat210_teamone.skolerute.model.SchoolVacationDay;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,10 +15,18 @@ import java.util.Date;
  */
 public class StorageInterfaceTest {
     @Test
-    public void TestRetrieveAll() throws Exception{
+    public void TestRetrieveAllSchools() throws Exception{
         StorageInterface si = new DummyStorage();
 
         SchoolInfo[] test = si.GetSchoolInfo();
+        Assert.assertEquals(10, test.length);
+    }
+
+    @Test
+    public void TestRetrieveAllVacationDays() throws Exception{
+        StorageInterface si = new DummyStorage();
+
+        SchoolVacationDay[] test = si.GetVacationDays();
         Assert.assertEquals(10, test.length);
     }
 }
