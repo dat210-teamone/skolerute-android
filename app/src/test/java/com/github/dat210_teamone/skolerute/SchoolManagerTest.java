e9c51280f74de2eb4579d580c9329bc53d27a722
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-09-21 08:38:51 +0200

    Init school mangager test class

--- app/src/test/java/com/github/dat210_teamone/skolerute/SchoolManagerTest.java
+++ app/src/test/java/com/github/dat210_teamone/skolerute/SchoolManagerTest.java
@@ -1,9 +1,18 @@
 package com.github.dat210_teamone.skolerute;
 
+import com.github.dat210_teamone.skolerute.data.DummyStorage;
+import com.github.dat210_teamone.skolerute.data.IStorage;
+import com.github.dat210_teamone.skolerute.data.SchoolManager;
+
+import org.junit.Test;
+
 /**
  * Created by MAIN on 21.09.2016.
  */
 
 public class SchoolManagerTest {
+    IStorage si = new DummyStorage();
+    public SchoolManagerTest() {
 
+    }
 }
