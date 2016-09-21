ee073f9327d0da74eca722d633b6dcd7291f43b7
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-09-21 09:11:51 +0200

    Added tests

--- app/src/test/java/com/github/dat210_teamone/skolerute/SchoolManagerTest.java
+++ app/src/test/java/com/github/dat210_teamone/skolerute/SchoolManagerTest.java
@@ -3,7 +3,10 @@ package com.github.dat210_teamone.skolerute;
 import com.github.dat210_teamone.skolerute.data.DummyStorage;
 import com.github.dat210_teamone.skolerute.data.IStorage;
 import com.github.dat210_teamone.skolerute.data.SchoolManager;
+import com.github.dat210_teamone.skolerute.model.SchoolInfo;
+import com.github.dat210_teamone.skolerute.model.SchoolVacationDay;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -12,9 +15,22 @@ import org.junit.Test;
 
 public class SchoolManagerTest {
     IStorage si = new DummyStorage();
+    SchoolManager sm;
     public SchoolManagerTest() {
-
+        this.sm = new SchoolManager(si);
     }
 
-    
+    @Test
+    public void TestGetSelectedSchools() {
+        SchoolInfo[] si = new SchoolInfo[10];
+        si = sm.GetSelectedSchools();
+        Assert.assertEquals(2,si.length);
+    }
+
+    @Test
+    public void TestGetSelectedSchoolDays() {
+        SchoolVacationDay[] si = new SchoolVacationDay[10];
+        si = sm.GetSelectedSchoolDays();
+        Assert.assertEquals(10,si.length);
+    }
 }
