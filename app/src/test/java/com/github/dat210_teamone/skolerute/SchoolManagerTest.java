cfcd3f4cab84cfb44b92e696355a041cadbb0382
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-09-21 09:41:26 +0200

    School manager test is working

--- app/src/test/java/com/github/dat210_teamone/skolerute/SchoolManagerTest.java
+++ app/src/test/java/com/github/dat210_teamone/skolerute/SchoolManagerTest.java
@@ -1,6 +1,8 @@
 package com.github.dat210_teamone.skolerute;
 
+import com.github.dat210_teamone.skolerute.data.DummySettingStorage;
 import com.github.dat210_teamone.skolerute.data.DummyStorage;
+import com.github.dat210_teamone.skolerute.data.ISettingStorage;
 import com.github.dat210_teamone.skolerute.data.IStorage;
 import com.github.dat210_teamone.skolerute.data.SchoolManager;
 import com.github.dat210_teamone.skolerute.model.SchoolInfo;
@@ -14,23 +16,34 @@ import org.junit.Test;
  */
 
 public class SchoolManagerTest {
-    IStorage si = new DummyStorage();
-    SchoolManager sm;
+
+    private SchoolManager sm;
+
     public SchoolManagerTest() {
-        this.sm = new SchoolManager(si);
+        IStorage ds = new DummyStorage();
+        ISettingStorage dss = new DummySettingStorage();
+        this.sm = new SchoolManager(ds,dss);
+    }
+
+    @Test
+    public void TestCheckName() {
+        boolean cn1 = sm.checkName("Skole 2");
+        boolean cn2 = sm.checkName("Skole 3");
+        Assert.assertEquals(true, cn1);
+        Assert.assertEquals(false, cn2);
     }
 
     @Test
     public void TestGetSelectedSchools() {
-        SchoolInfo[] si = new SchoolInfo[10];
-        si = sm.GetSelectedSchools();
+        SchoolInfo[] si = new SchoolInfo[2];
+        si = sm.getSelectedSchools();
         Assert.assertEquals(2,si.length);
     }
 
     @Test
     public void TestGetSelectedSchoolDays() {
-        SchoolVacationDay[] si = new SchoolVacationDay[10];
-        si = sm.GetSelectedSchoolDays();
-        Assert.assertEquals(10,si.length);
+        SchoolVacationDay[] svd = new SchoolVacationDay[10];
+        svd = sm.getSelectedSchoolDays();
+        Assert.assertEquals(10,svd.length);
     }
 }
