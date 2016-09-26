5a985325f70a94fe7b8badd95df8311161d83d41
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-09-26 11:33:29 +0200

    cleand upp some code and added new test

--- app/src/main/java/com/github/dat210_teamone/skolerute/data/CsvUrlGetter.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/data/CsvUrlGetter.java
@@ -6,8 +6,6 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
