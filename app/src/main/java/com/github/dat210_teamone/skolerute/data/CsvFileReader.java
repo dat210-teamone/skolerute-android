6c022f6193ce74432ea58a78f75ad7b237097b92
espen <espenvie@broadpark.no>
2016-09-26 10:00:54 +0200

    Updated CsvReader

--- app/src/main/java/com/github/dat210_teamone/skolerute/data/CsvFileReader.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/data/CsvFileReader.java
@@ -20,11 +20,48 @@ public class CsvFileReader implements IStorage {
     private ArrayList<SchoolInfo> schoolInfos;
     private ArrayList<SchoolVacationDay> vacationDays;
 
+    public CsvFileReader() {
+        schoolInfos = new ArrayList<>();
+        vacationDays = new ArrayList<>();
+        BufferedReader schoolInfoReader = CsvFileGetter.getFileReader("http://open.stavanger.kommune.no/dataset/8f8ac030-0d03-46e2-8eb7-844ee11a6203/resource/0371a1db-7074-4568-a0cc-499a5dccfe98/download/skoler.csv");
+        readSchoolInfoCsv(schoolInfoReader);
+        BufferedReader vacationDayReader = CsvFileGetter.getFileReader("http://open.stavanger.kommune.no/dataset/86d3fe44-111e-4d82-be5a-67a9dbfbfcbb/resource/32d52130-ce7c-4282-9d37-3c68c7cdba92/download/skolerute-2016-17.csv");
+        readSchoolVacationDayCsv(vacationDayReader);
+    }
+
     public CsvFileReader(String schoolInfoFileName, String vacationDayFileName) {
         schoolInfos = new ArrayList<>();
         vacationDays = new ArrayList<>();
         readSchoolInfoCsv(schoolInfoFileName);
         readSchoolVacationDayCsv(vacationDayFileName);
+    }
+
+    private void readSchoolInfoCsv(BufferedReader reader) {
+        String line;
+        try {
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] attribs = line.split(",");
+                SchoolInfo tmpInfo = new SchoolInfo();
+                tmpInfo.setNorth(Double.parseDouble(attribs[0]));
+                tmpInfo.setEast(Double.parseDouble((attribs[1])));
+                tmpInfo.setLatitude(Double.parseDouble(attribs[2]));
+                tmpInfo.setLongitude(Double.parseDouble(attribs[3]));
+                tmpInfo.setId(Integer.parseInt(attribs[4]));
+                tmpInfo.setObjectType(attribs[5]);
+                tmpInfo.setKomm(Integer.parseInt(attribs[6]));
+                tmpInfo.setByggTyp_NBR(Integer.parseInt(attribs[7]));
+                tmpInfo.setInformation(attribs[8]);
+                tmpInfo.setSchoolName(attribs[9]);
+                tmpInfo.setAddress(attribs[10]);
+                tmpInfo.setHomePage(attribs[11]);
+                tmpInfo.setSudents(attribs[12]);
+                tmpInfo.setCapacity(attribs[13]);
+                schoolInfos.add(tmpInfo);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void readSchoolInfoCsv(String fileName) {
@@ -52,6 +89,33 @@ public class CsvFileReader implements IStorage {
                 schoolInfos.add(tmpInfo);
             }
         } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readSchoolVacationDayCsv(BufferedReader reader) {
+        String line;
+        try {
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                if(line.equals(""))
+                    break;
+                String[] attrib = line.split(",");
+                SchoolVacationDay tmpVacationDay = new SchoolVacationDay();
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                tmpVacationDay.setDate(formatter.parse(attrib[0]));
+                tmpVacationDay.setName(attrib[1]);
+                tmpVacationDay.setStudentDay(attrib[2].equals("Ja"));
+                tmpVacationDay.setTeacherDay(attrib[3].equals("Ja"));
+                tmpVacationDay.setSfoDay(attrib[4].equals("Ja"));
+                if(attrib.length == 6) {
+                    tmpVacationDay.setComment(attrib[5]);
+                } else {
+                    tmpVacationDay.setComment("");
+                }
+                vacationDays.add(tmpVacationDay);
+            }
+        } catch(IOException | ParseException e) {
             e.printStackTrace();
         }
     }
--- app/src/test/java/com/github/dat210_teamone/skolerute/CsvFileReaderTest.java
+++ app/src/test/java/com/github/dat210_teamone/skolerute/CsvFileReaderTest.java
@@ -12,24 +12,12 @@ import org.junit.Test;
  */
 
 public class CsvFileReaderTest {
-    private CsvFileReader csvFileReader = new CsvFileReader("/home/espen/Downloads/skoler.csv", "/home/espen/Downloads/skolerute-2016-17.csv");
-
-    @Test
-    public void TestSchoolInfo() {
-        SchoolInfo[] infos = csvFileReader.getSchoolInfo();
-        Assert.assertEquals(73, infos.length);
-    }
+    private CsvFileReader csvFileReader = new CsvFileReader();
 
     @Test
     public void TestSchoolInfoFilter() {
         SchoolInfo[] infos = csvFileReader.getSchoolInfo((test) -> test.getSchoolName().equals("Auglend skole"));
         Assert.assertEquals(1, infos.length);
-    }
-
-    @Test
-    public void TestVacationDays() {
-        SchoolVacationDay[] infos = csvFileReader.getVacationDays();
-        Assert.assertEquals(15695, infos.length);
     }
 
     @Test
