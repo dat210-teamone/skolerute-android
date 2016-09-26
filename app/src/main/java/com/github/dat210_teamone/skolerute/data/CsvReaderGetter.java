abfe24e48746adb80dc4c166310b32d7308aa900
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2016-09-26 12:56:34 +0200

    Page Info implementation

--- app/src/main/java/com/github/dat210_teamone/skolerute/data/CsvReaderGetter.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/data/CsvReaderGetter.java
@@ -1,5 +1,7 @@
 package com.github.dat210_teamone.skolerute.data;
 
+import com.github.dat210_teamone.skolerute.model.PageInfo;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -7,16 +9,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
 
 /**
  * Created by Fredrik Wigsnes on 26.09.2016.
  */
 
 public class CsvReaderGetter implements ICsvGetter {
-
+    private static HashMap<String, PageInfo> infoCache = new HashMap<>();
     static BufferedReader getFileReader(String url) {
         try {
             URL u = new URL(url);
+            //String test = u.getFile();
             InputStream input = u.openStream();
             return new BufferedReader(new InputStreamReader(input,"UTF-8"));
         } catch (IOException e) {
@@ -24,6 +28,35 @@ public class CsvReaderGetter implements ICsvGetter {
             System.out.println(e.getMessage());
         }
         return null;
+    }
+
+    public static PageInfo getInfo(String url)
+    {
+        if (infoCache.containsKey(url))
+            return infoCache.get(url);
+        else
+        {
+            try {
+                URL u = new URL(url);
+                InputStream input = u.openStream();
+                DataInputStream dis = new DataInputStream(new BufferedInputStream(input));
+                byte[] bytes =  new byte[input.available()];
+                dis.readFully(bytes);
+                String s = new String(bytes);
+                String searchString = "class=\"resource-item\" data-id=\"";
+                int i = s.lastIndexOf(searchString);
+                int begin = s.indexOf("<a href=\"http://open.stavanger.kommune.no/", i) + 9;
+                int end = s.indexOf('"', begin);
+
+
+                PageInfo info = new PageInfo(url, s.substring(begin, end), "");
+
+                infoCache.put(url, info);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return new PageInfo(url, null, null);
+        }
     }
 
     public static String getFileUrl(String url) {
