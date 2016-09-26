930c4fc973364d3292b43be643b57945e633a22c
jerzyjje <jerzyjje@gmail.com>
2016-09-26 08:34:25 +0200

    SearchView

--- app/src/main/java/com/github/dat210_teamone/skolerute/MainActivity.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/MainActivity.java
@@ -3,12 +3,13 @@ package com.github.dat210_teamone.skolerute;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.View;
-
-import com.github.dat210_teamone.skolerute.data.DummyStorage;
-
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SearchView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void changeActivity(View view) {
+    public void findSSchool(View view) {
         Intent myIntent = new Intent(this, AddSchoolActivity.class);
         startActivity(myIntent);
     }
 
     public void findSchool(View view) {
-        Intent myIntent = new Intent(this, AddSchoolActivity.class);
+        Intent myIntent = new Intent(this, NearestSchool.class);
         startActivity(myIntent);
     }
 
