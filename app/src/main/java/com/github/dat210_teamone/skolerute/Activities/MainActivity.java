ba29099d6d54c635d25a21d72f8c89f094d9d50a
Alexander Wiig Sørensen <alex.ws@icloud.com>
2016-09-26 09:32:02 +0200

    Endret fra activities til fragments

---
+++ app/src/main/java/com/github/dat210_teamone/skolerute/Activities/MainActivity.java
@@ -0,0 +1,81 @@
+package com.github.dat210_teamone.skolerute.Activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.github.dat210_teamone.skolerute.Fragments.AddSchools;
+import com.github.dat210_teamone.skolerute.Fragments.CalendarList;
+import com.github.dat210_teamone.skolerute.Fragments.CalendarStandard;
+import com.github.dat210_teamone.skolerute.Fragments.StoredSchools;
+import com.github.dat210_teamone.skolerute.R;
+
+
+public class MainActivity extends AppCompatActivity implements AddSchools.OnAddSchoolsInteractionListener, CalendarList.OnCalendarListInteractionListener, StoredSchools.OnStoredSchoolsInteractionListener, CalendarStandard.OnCalendarStandardInteractionListener{
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            if(5>2) {
+                fragment = new AddSchools();
+            } else {
+                fragment = new StoredSchools();
+            }
+
+            FragmentTransaction frag_trans =  manager.beginTransaction();
+            // Add container and fragment for the container
+            frag_trans.add(R.id.fragment_container, fragment);
+            frag_trans.commit();
+        }
+    }
+
+
+
+
+
+    public void showSchools(View view) {
+
+    }
+
+
+
+
+
+    // Abstract methods from fragments
+    @Override
+    public void onAddSchoolsInteraction(Uri uri){
+
+    }
+
+    @Override
+    public void onCalendarListInteraction(Uri uri){
+
+    }
+
+    @Override
+    public void onStoredSchoolsInteraction(Uri uri){
+
+    }
+
+    @Override
+    public void onCalendarStandardInteraction(Uri uri){
+
+    }
+
+}
