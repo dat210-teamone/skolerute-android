46fd18e7bdbada9a9f3e396b08f9d8eef430f69b
espen <espenvie@broadpark.no>
2016-09-26 10:38:39 +0200

    Removed redundant code

--- app/src/main/java/com/github/dat210_teamone/skolerute/data/CsvFileReader.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/data/CsvFileReader.java
@@ -66,29 +66,9 @@ public class CsvFileReader implements IStorage {
     }
 
     private void readSchoolInfoCsv(String fileName) {
-        String line;
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
-            br.readLine();
-            while((line = br.readLine()) != null) {
-                String[] attribs = line.split(",");
-                SchoolInfo tmpInfo = new SchoolInfo();
-                tmpInfo.setNorth(Double.parseDouble(attribs[0]));
-                tmpInfo.setEast(Double.parseDouble((attribs[1])));
-                tmpInfo.setLatitude(Double.parseDouble(attribs[2]));
-                tmpInfo.setLongitude(Double.parseDouble(attribs[3]));
-                tmpInfo.setId(Integer.parseInt(attribs[4]));
-                tmpInfo.setObjectType(attribs[5]);
-                tmpInfo.setKomm(Integer.parseInt(attribs[6]));
-                tmpInfo.setByggTyp_NBR(Integer.parseInt(attribs[7]));
-                tmpInfo.setInformation(attribs[8]);
-                tmpInfo.setSchoolName(attribs[9]);
-                tmpInfo.setAddress(attribs[10]);
-                tmpInfo.setHomePage(attribs[11]);
-                tmpInfo.setSudents(attribs[12]);
-                tmpInfo.setCapacity(attribs[13]);
-                schoolInfos.add(tmpInfo);
-            }
+            readSchoolInfoCsv(br);
         } catch(IOException e) {
             e.printStackTrace();
         }
@@ -123,29 +103,10 @@ public class CsvFileReader implements IStorage {
     }
 
     private void readSchoolVacationDayCsv(String fileName) {
-        String line;
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
-            br.readLine();
-            while ((line = br.readLine()) != null) {
-                if(line.equals(""))
-                    break;
-                String[] attrib = line.split(",");
-                SchoolVacationDay tmpVacationDay = new SchoolVacationDay();
-                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-                tmpVacationDay.setDate(formatter.parse(attrib[0]));
-                tmpVacationDay.setName(attrib[1]);
-                tmpVacationDay.setStudentDay(attrib[2].equals("Ja"));
-                tmpVacationDay.setTeacherDay(attrib[3].equals("Ja"));
-                tmpVacationDay.setSfoDay(attrib[4].equals("Ja"));
-                if(attrib.length == 6) {
-                    tmpVacationDay.setComment(attrib[5]);
-                } else {
-                    tmpVacationDay.setComment("");
-                }
-                vacationDays.add(tmpVacationDay);
-            }
-        } catch(IOException | ParseException e) {
+            readSchoolVacationDayCsv(br);
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
