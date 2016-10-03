0b103a2f7efa1756eea2b5f1450a017c4ca39750
jerzyjje <jerzyjje@gmail.com>
2016-10-03 11:19:48 +0200

    CalendarStandart

--- app/src/main/java/com/github/dat210_teamone/skolerute/Fragments/CalendarStandard.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/Fragments/CalendarStandard.java
@@ -9,13 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.github.dat210_teamone.skolerute.Activities.MainActivity;
 import com.github.dat210_teamone.skolerute.R;
+import com.github.dat210_teamone.skolerute.adapters.CalendarViewer;
 import com.github.dat210_teamone.skolerute.model.SchoolInfo;
 import com.github.dat210_teamone.skolerute.model.SchoolVacationDay;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,13 +31,13 @@ import java.util.Date;
  * create an instance of this fragment.
  */
 public class CalendarStandard extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private CalendarView calView;
-    // TODO: Rename and change types of parameters
+    private CalendarViewer calView;
+    private HashSet<Date> events ;
+
     private String mParam1;
     private String mParam2;
 
@@ -75,17 +80,31 @@ public class CalendarStandard extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_calendar_standard, container, false);
 
+        events = new HashSet<>();
+        events.add(new Date());
+
         MainActivity mainActivity = (MainActivity)getActivity();
 
         SchoolInfo school=mainActivity.selectedSchools[0];
         SchoolVacationDay vacationDays[] = mainActivity.schoolManager.getSelectedSchoolDays();
         Date[] days=new Date[vacationDays.length];
 
+        calView= ((CalendarViewer)view.findViewById(R.id.cal_view));
+        calView.updateCalendar(events);
+
+
         for (int x=0; x<vacationDays.length; x++){
             days[x]=vacationDays[x].getDate();
         }
 
-        calView = (CalendarView) view.findViewById(R.id.cal_view);
+        calView.setEventHandler(new CalendarViewer.EventHandler() {
+            @Override
+            public void onDayLongPress(Date date)
+            {
+                DateFormat df = SimpleDateFormat.getDateInstance();
+                Toast.makeText(mainActivity, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
