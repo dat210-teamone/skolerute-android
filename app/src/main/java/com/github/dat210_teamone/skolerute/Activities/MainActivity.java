e1a4e806dccff6997ea379d1b0ff492c14355dd5
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2016-10-12 10:55:52 +0200

    Removed line

--- app/src/main/java/com/github/dat210_teamone/skolerute/Activities/MainActivity.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/Activities/MainActivity.java
@@ -28,7 +28,7 @@ import com.github.dat210_teamone.skolerute.data.InterfaceManager;
 
 import com.github.dat210_teamone.skolerute.data.NotificationUtil;
 
-import com.github.dat210_teamone.skolerute.data.positionService.LocationFinder;
+//import com.github.dat210_teamone.skolerute.data.positionService.LocationFinder;
 
 import com.github.dat210_teamone.skolerute.data.SchoolManager;
 import com.github.dat210_teamone.skolerute.model.SchoolInfo;
