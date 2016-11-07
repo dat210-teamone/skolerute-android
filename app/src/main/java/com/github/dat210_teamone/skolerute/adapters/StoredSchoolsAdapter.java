e84f176c8a7a021f95d5c943f00c8b80ac4d8fe1
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-11-07 08:54:01 +0100

    fixed app crash bug

--- app/src/main/java/com/github/dat210_teamone/skolerute/adapters/StoredSchoolsAdapter.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/adapters/StoredSchoolsAdapter.java
@@ -42,6 +42,7 @@ public class StoredSchoolsAdapter extends ArrayAdapter<String> {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.stored_schools_layout, parent, false);
+        View schoolView = inflater.inflate(R.layout.fragment_stored_schools, parent, false);
         TextView schoolName = (TextView) rowView.findViewById(R.id.school_name);
         //      TextView nextDate = (TextView) rowView.findViewById(R.id.next_date);
         LinearLayout schoolNameContainer = (LinearLayout)rowView.findViewById(R.id.school_name_container);
@@ -79,12 +80,7 @@ public class StoredSchoolsAdapter extends ArrayAdapter<String> {
 
         //SCHOOLSETTINGS
         ImageView schoolSettingsBtn = (ImageView) rowView.findViewById(R.id.stored_schools_item_menu);
-        ImageView settingBtn = (ImageView) rowView.findViewById(R.id.stored_schools_menu);
-
-        /*//POPUP
-        PopupMenu settingsMenu = new PopupMenu(context, schoolSettingsBtn); // Usikker på om dette er slik du gjør det.
-        //POPUP END*/
-
+        ImageView settingBtn = (ImageView) schoolView.findViewById(R.id.stored_schools_menu);
 
         schoolSettingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
