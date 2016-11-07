67ed27a9a0bba15d84f1e3bafab1ae0a1199f1ea
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-11-07 10:23:36 +0100

    fixed bug global notific

--- app/src/main/java/com/github/dat210_teamone/skolerute/Fragments/StoredSchools.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/Fragments/StoredSchools.java
@@ -112,19 +112,6 @@ public class StoredSchools extends Fragment {
 
         setupPopupMenu(view, mainActivity);
 
-        ImageView settingBtn = (ImageView) view.findViewById(R.id.stored_schools_menu);
-
-        settingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu settingsMenu = new PopupMenu(mainActivity, settingBtn);
-
-                settingsMenu.inflate(R.menu.stored_school_popup_menu);
-                settingsMenu.show();
-            }
-        });
-
-
         return view;
     }
 
