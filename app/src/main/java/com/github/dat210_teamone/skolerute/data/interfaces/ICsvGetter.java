f6dade73dfa9c5bb735b237dea4e5b7d9bc25bf9
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2016-11-30 15:11:20 +0100

    Bug fixing and clean up

--- app/src/main/java/com/github/dat210_teamone/skolerute/data/interfaces/ICsvGetter.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/data/interfaces/ICsvGetter.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
  * Created by Nicolas on 26.09.2016.
  */
 
+@Deprecated
 public interface ICsvGetter {
     BufferedReader getSchoolReader();
     BufferedReader getSchoolDayReader();
