6bce154a5a7ce435427ddae224e3e1d4b5c38bcb
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2016-12-03 18:51:59 +0100

    Line shift

--- app/src/main/java/com/github/dat210_teamone/skolerute/Fragments/CalendarStandard.java
+++ app/src/main/java/com/github/dat210_teamone/skolerute/Fragments/CalendarStandard.java
@@ -31,8 +31,7 @@ public class CalendarStandard extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_calendar_standard, container, false);
 
